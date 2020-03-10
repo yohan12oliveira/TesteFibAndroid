c00b5680cb22e21f38c87c5ce767da3b1cbc52f9
yohan12oliveira <navarroyohan66outlook.com>
2020-03-09 22:17:52 -0300

    aula2

--- testeandroid/app/src/main/java/br/aula/testeandroid/MainActivity.kt
+++ testeandroid/app/src/main/java/br/aula/testeandroid/MainActivity.kt
@@ -1,22 +1,24 @@
 package br.aula.testeandroid
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_basico.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setContentView(R.layout.activity_main_basico)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+       // texto1.text
+        // texto2.text
+        botao1.setOnClickListener{
+            Toast.makeText( this@MainActivity,
+                "Apertei o login", Toast.LENGTH_LONG).show();
+        }
         }
     }
 
-}
+
